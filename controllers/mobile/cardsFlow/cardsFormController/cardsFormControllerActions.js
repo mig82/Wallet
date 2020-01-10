ef09dfff7bb17f184b4687aa0691ffe2f3275f3b
Miguelángel Fernández <miguelangelxfm@gmail.com>
2020-01-10 10:17:29 +0100

    Import cards form from DBX native mobile project
    This includes stuff we won't need:
    * A second BlueTheme theme.
    * Actions with weird names.
    * Skins with weird names.
    * A bunch of images.

--- controllers/mobile/cardsFlow/cardsFormController/cardsFormControllerActions.js
+++ controllers/mobile/cardsFlow/cardsFormController/cardsFormControllerActions.js
@@ -1,7 +1,25 @@
-define({ 
-
- /* 
- This is an auto generated file and any modifications to it may result in corruption of the action sequence.
- */
-
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for undefined **/
+    AS_BarButtonItem_a5d38c337107466abacb644cd404d7f9: function AS_BarButtonItem_a5d38c337107466abacb644cd404d7f9(eventobject) {
+        var self = this;
+        this.navigateToCardMngDetails();
+    },
+    /** init defined for frmCardManageHome **/
+    AS_Form_jb0467d0e2a4488cb39e5934f477fc9a: function AS_Form_jb0467d0e2a4488cb39e5934f477fc9a(eventobject) {
+        var self = this;
+        this.init();
+    },
+    /** preShow defined for frmCardManageHome **/
+    AS_Form_b14bc3b2043c4291b012d3026854b60e: function AS_Form_b14bc3b2043c4291b012d3026854b60e(eventobject) {
+        var self = this;
+        return self.preShow.call(this);
+    },
+    /** postShow defined for frmCardManageHome **/
+    AS_Form_i39294aa1ad845a8bcbf2b6b968932fa: function AS_Form_i39294aa1ad845a8bcbf2b6b968932fa(eventobject) {
+        var self = this;
+        this.postShow();
+    }
 });
