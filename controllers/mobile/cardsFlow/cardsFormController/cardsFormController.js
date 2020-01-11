8c1fe273fa38c24086d797971db9d65d6f06dc9f
Miguelángel Fernández <miguelangelxfm@gmail.com>
2020-01-11 02:27:58 +0100

    Fix cardsFormController to use updated libs
    Use kony.animations.reveal and Promise instead of Q.Promise.

--- controllers/mobile/cardsFlow/cardsFormController/cardsFormController.js
+++ controllers/mobile/cardsFlow/cardsFormController/cardsFormController.js
@@ -7,15 +7,15 @@ define(function(){
 		fetchCards: function(){
 			//TODO: Fetch these from the server.
 			var holder = "František Koudelka";
-			/*globals Q*/
-			return Q.when([
+
+			return Promise.resolve([
 				{
 					type: kony.i18n.getLocalizedString2("product.CREDIT_CARD"),
 					holder,
 					balance: 31084.61,
 					limit: 100000,
 					currency: "CZK",
-					image: "credit_card.png",
+					image: "card_plastic_1.png",
 					pan: "5000 0012 3456 1699"
 				},{
 					type: kony.i18n.getLocalizedString2("product.CREDIT_CARD_CUSTOM"),
@@ -23,8 +23,8 @@ define(function(){
 					balance: 125.78,
 					limit: 2500,
 					currency: "EUR",
-					image: "credit_card_custom_2.png",
-					pan: "5000 0012 3477 9737"
+					image: "card_plastic_2.png",
+					pan: "4000 0012 3477 9737"
 				}
 			]);
 		},
@@ -55,23 +55,29 @@ define(function(){
 		},
 
 		showElements: function(){
-			/*globals reveal*/
 			if(kony.os.isIos()){
-				reveal(this.view.blockButton, 0.5, 0.20);
-				reveal(this.view.detailsButton, 0.5, 0.40);
-				reveal(this.view.settingsButton, 0.5, 0.60);
+				kony.animations.reveal(this.view.blockButton, 0.5, 0.20);
+				kony.animations.reveal(this.view.detailsButton, 0.5, 0.40);
+				kony.animations.reveal(this.view.settingsButton, 0.5, 0.60);
 			}
 			else{
-				reveal(this.view.buttonsContainer);
+				kony.animations.reveal(this.view.buttonsContainer);
 			}
-			reveal(this.view.linkedAccountFlex, 0.5, 0.80);
+			kony.animations.reveal(this.view.linkedAccountFlex, 0.5, 0.80);
 		},
 
+		init: function(){
+			//Stuff you only want done once the first time the screen is visited.
+		},
 		preShow: function(){
+			//Move stuff out of sight if you want to then animate back in.
 			this.hideElements();
 		},
 
 		postShow: function() {
+			//Call services to populate screen.
+			//Animate stuff back into sight.
+
 			this.fetchCards()
 			.then((cards) => {
 				kony.print("Cards: "+ JSON.stringify(cards));
@@ -84,8 +90,13 @@ define(function(){
 			this.showElements();
 		},
 
+		onHide: function(){
+			//Make sure you destroy/empty large variables that won't be needed anymore.
+		},
+
 		onNavigate: function(){
-			kony.mvc.patch(this);
+			//Wire it all together.
+			kony.mvc.wire(this);
 		}
 
 	};
