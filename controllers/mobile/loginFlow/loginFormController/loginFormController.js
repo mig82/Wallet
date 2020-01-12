02a8cf31a1da847fd73a84a0d6d62c071cc53969
Miguelángel Fernández <miguelangelxfm@gmail.com>
2020-01-12 03:51:33 +0100

    Wire login and cards services

--- controllers/mobile/loginFlow/loginFormController/loginFormController.js
+++ controllers/mobile/loginFlow/loginFormController/loginFormController.js
@@ -1,5 +1,30 @@
-define({ 
+define(function(){
 
- //Type your controller code here 
+	return{
 
- });
+		preShow: function(){
+			//Move stuff out of sight if you want to then animate back in.
+			this.view.loginButton.hideButton();
+		},
+
+		postShow: function(){
+			//Call services to populate screen.
+			//Animate stuff back into sight.
+			this.view.loginButton.showButton(0.5, 0.2);
+
+			const login = require("loginFlow/login");
+			this.view.loginButton.onPressed = () => {
+				login({}, true)
+				.then((profile) => {
+					alert("Navigating to cards");
+					kony.router.goto("cards", {}, true);
+				});
+			};
+		},
+
+		onNavigate: function(){
+			//Wire it all together.
+			kony.mvc.wire(this);
+		}
+	};
+});
--- modules/require/cardsFlow/fetchCards.js
+++ modules/require/cardsFlow/fetchCards.js
@@ -1,5 +1,7 @@
 define(function () {
 
+	const service = "cards_miguel";
+	const operation = "get_cards";
 	var cards;
 
 	/*Fetch cards from server, or locally if already queried before.
@@ -9,7 +11,7 @@ define(function () {
 		if(typeof cards === "undefined" || force){
 			kony.print("cardsFlow/fetchCards: Fetching cards from server");
 			//TODO: Fetch these from the server.
-			cards = [
+			/*cards = [
 				{
 					type: kony.i18n.getLocalizedString2("Credit Card"),
 					holder: "James M. Hendrix",
@@ -27,12 +29,47 @@ define(function () {
 					image: "card_plastic_2.png",
 					pan: "4000 0012 3477 9737"
 				}
-			];
+			];*/
+
+			return new Promise((resolve, reject) => {
+
+				try{
+					kony.sdk.getCurrentInstance()
+					.getIntegrationService(service)
+					.invokeOperation(
+						operation,
+						{}, //headers
+						{}, //data
+						(response) => { //onSuccess
+							if(response.opstatus === 0 || response.opstatus === "0"){
+								cards = response.cards;
+								resolve(cards);
+							}
+							else{
+								reject(response);
+							}
+						},
+						(error) => { //onFailure
+							reject(error);
+						},
+						{} //options
+					);
+				}
+				catch(e){
+					var errorMsg = e.details.errmsg;
+					if (errorMsg.indexOf('Invalid session') >= 0 || errorMsg.indexOf('session expired') >= 0) {
+						reject(e);
+					}
+					else{
+						reject(new Error(`Could not find or call ${service}.${operation}:\n\t${e}`));
+					}
+				}
+			});
 		}
 		else{ //We already have the cards and are not being asked to fetch them again.
 			kony.print("cardsFlow/fetchCards: Fetching cards locally");
+			return Promise.resolve(cards);
 		}
-		return Promise.resolve(cards);
 	}
 
     return fetchCards;
