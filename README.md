4366060006bf1ed15cbdc490228d6799ac268af8
mig82 <miguelangelxfm@gmail.com>
2020-05-26 22:39:08 +0200

    Test selecting first card, and swiping forth and back

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wallet
+
+A sample Temenos (Formerly Kony) [Quantum](https://www.kony.com/quantum/) application showcasing some coding best practices and component design principles.
---
+++ testresources/Jasmine/Mobile/Test Cases/selectFirstCard.js
@@ -0,0 +1,10 @@
+it("selectFirstCard", async function() {
+	await kony.automation.playback.wait(3000);
+
+	//Verify type, holder and balance of first card.
+	expect(kony.automation.widget.getWidgetProperty(["cardsForm","CardInfo","typeLabel"], "text")).toEqual("Credit Card");
+	expect(kony.automation.widget.getWidgetProperty(["cardsForm","CardInfo","holderLabel"], "text")).toEqual("James M. Hendrix");
+	expect(kony.automation.widget.getWidgetProperty(["cardsForm","CardInfo","balaneLabel"], "text")).toEqual("CZK31,084.61");
+
+	kony.automation.button.click(["cardsForm","cardOptions1","detailsButton","button1"]);
+});
