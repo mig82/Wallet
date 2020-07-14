093d6dd2a653600baf171a1fd669225748e6a1ea
mig82 <miguelangelxfm@gmail.com>
2020-07-14 19:28:17 +0200

    Add splash screen and internal login option
    No internal login form yet. This is just a placeholder.

--- controllers/mobile/authModule/loginFormController/loginFormController.js
+++ controllers/mobile/authModule/loginFormController/loginFormController.js
@@ -5,13 +5,20 @@ define(function(){
 		preShow: function(){
 			//Move stuff out of sight if you want to then animate back in.
 			this.view.loginButton.hideButton();
+			this.view.extLoginButton.hideButton();
 		},
 
 		postShow: function(){
 			//Call services to populate screen.
 			//Animate stuff back into sight.
 			this.view.loginButton.showButton(0.5, 0.2);
+			this.view.extLoginButton.showButton(0.5, 0.4);
+
 			this.view.loginButton.onPressed = () => {
+				kony.router.goto("internalLogin", {}, true);
+			};
+
+			this.view.extLoginButton.onPressed = () => {
 				kony.router.goto("externalLogin", {}, true);
 			};
 		},
